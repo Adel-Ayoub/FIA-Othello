116d3b98814858b4edb7fab512b052aada4d5e88
adelayoub <adelayoub.maaziz@gmail.com>
2025-10-07 19:26:53 +0100

    feat: add main.rs entry point with module declarations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod board;
+mod common;
+mod game;
+mod referee;
+mod statistics;
+
+use eframe::egui;
+use game::Game;
+
 fn main() {
-    println!("Mazal lwekt");
+    let app = Game::default();
+    let _ = eframe::run_native(
+        "Othello",
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default().with_inner_size([1024.0, 768.0]),
+            ..Default::default()
+        },
+        Box::new(|_cc| Ok(Box::new(app))),
+    );
 }
